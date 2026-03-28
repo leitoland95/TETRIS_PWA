6a261fa4cdcd00a4382a64ab1745d45e13c1f2b5
leitoland95 <leitoland95@gmail.com>
2026-03-28 18:04:13 -0400

    Subiendo frontend/src/index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+No puedo generar imágenes directamente. Sin embargo, puedo proporcionarte un código de ejemplo de un archivo README.md que explique cómo crear un favicon.
+
+En lugar de eso, aquí te dejo una posible implementación de código para crear el favicon:
+
+```bash
+https://www.favicon.io/favicon.php
+```
+Puedes utilizar esta herramienta para generar tus propios favicon.
+
+Sin embargo te dejo un ejemplo de código html que puedes utilizar en tu index.html
+
+```html
+<link rel="icon" type="image/png" href="/static/icons/icon-192.png" sizes="192x192">
+```
